bd5f8e9b78fa1ebc1de1345580569ef1ee7d8f4d
Pablo Duboue <pablo.duboue@gmail.com>
2016-09-16 18:27:24 -0400

    Made it robust

--- dovetail.pl
+++ dovetail.pl
@@ -11,16 +11,31 @@ foreach my$arg(0..$#ARGV){
     my@f=<F>;
     chomp@f;
     push@datasets,[ map { [ split(/\s+/, $_) ] } @f  ];
+#    print STDERR "Read ".scalar(@{$datasets[-1]})."\n";
+    close F;
 }
+#die;
 
-foreach my$row(0..(scalar($datasets[0]) - 1)){
+foreach my$row(0..(scalar(@{$datasets[0]}) - 1)){
     my$base = $datasets[0]->[$row];
-    print "$base->[0]\t";
-    foreach my$ds(1..$#datasets){
-	die "mismatch: $ds $row" unless $datasets[$ds]->[$row]->[0] eq $datasets[0]->[$row]->[1];
-	print $datasets[$ds]->[$row]->[1]."\t";
+    if($base->[0]){
+      print "$base->[0]\t";
+      foreach my$ds(1..$#datasets){
+        my$label="O";
+        if($datasets[$ds]->[0]->[0]){
+          die "mismatch: $ds $row" unless $datasets[$ds]->[0]->[0] eq $datasets[0]->[$row]->[1];
+          $label=$datasets[$ds]->[0]->[1];
+          shift@{$datasets[$ds]};
+        } # else error recovery
+        print "$label\t"; 
+      }
+      print "$base->[1]\n";
+    }else{
+      foreach my$ds(1..$#datasets){
+        shift@{$datasets[$ds]};
+      }
+      print "\n";
     }
-    print "$base->[1]\n";
 }
 
     
